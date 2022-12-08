ff18427ca08cc617b763c05e30880852906d613c
apirrone <awanegenebayo@gmail.com>
2022-12-08 19:20:12 +0100

    2022d6

---
+++ 2022/day_six/p2.py
@@ -0,0 +1,19 @@
+data = open("input.txt", 'r').read()
+print(data)
+
+def allDiff(substr):
+    for i in range(len(substr)):
+        a = substr[i]
+        for j in range(i+1, len(substr)):
+            b = substr[j]
+            if a == b:
+                return False
+
+    return True
+
+for i in range(0, len(data)-3):
+    substr = data[i:i+14]
+    val = allDiff(substr)
+    if val:
+        print(i+14, substr)
+        exit()
